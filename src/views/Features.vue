3a6e8f3f95582b2ab62a6c352ff7832fe14d56cb
Derek Haus <rumbleincybertron@gmail.com>
2020-01-16 15:07:22 +0100

    Draft
    TODO: Improve AboutUs
    Revise text where needed.

--- src/views/Features.vue
+++ src/views/Features.vue
@@ -11,6 +11,79 @@
       <PatternBG text="Features" repeat="3" />
       <div class="left">
         <div class="feature-short">
+          <div class="text">Heat Maps</div>
+          <div class="circle"></div>
+        </div>
+        <div class="feature-short middle">
+          <div class="text">Rating System</div>
+          <div class="circle"></div>
+        </div>
+        <div class="feature-short">
+          <div class="text">Trip Advising System</div>
+          <div class="circle"></div>
+        </div>
+      </div>
+
+      <img class="mockup" src="../assets/mockup.png" />
+
+      <div class="right">
+        <div class="feature-short">
+          <div class="text">Journey History</div>
+          <div class="circle"></div>
+        </div>
+        <div class="feature-short middle">
+          <div class="text">Travel Info/Warnings</div>
+          <div class="circle"></div>
+        </div>
+        <div class="feature-short">
+          <div class="text">New! Rate your Conductor</div>
+          <div class="circle"></div>
+        </div>
+      </div>
+
+      <div class="square first"></div>
+      <div class="square second"></div>
+    </div>
+
+    <!-- Features in details -->
+    <div class="features-details-container">
+      <div class="square"></div>
+      <FeatureDetails
+        v-for="(feature, index) in $store.state.features"
+        :key="feature.name"
+        :name="feature.name"
+        :description="feature.description"
+        :illustrationURL="feature.illustrationURL"
+        :color="index%2==0 ? 'first' : 'second'"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import PatternBG from "./../components/common/PatternBG";
+import Heading from "./../components/common/Heading";
+import FeatureDetails from "./../components/features-components/FeatureDetails.vue";
+export default {
+  components: { PatternBG, FeatureDetails, Heading }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/features.scss";
+</style><template>
+  <div id="features-page">
+    <!-- Heading -->
+    <Heading pretext="Features">
+      We've packed a lot into this.
+      <br />Take a look at the features available.
+    </Heading>
+
+    <!-- Features -->
+    <div class="features-overview">
+      <PatternBG text="Features" repeat="3" />
+      <div class="left">
+        <div class="feature-short">
           <div class="text">Feature here</div>
           <div class="circle"></div>
         </div>
--- src/views/LearnMore.vue
+++ src/views/LearnMore.vue
@@ -1,85 +1,75 @@
 <template>
-  <div id="learn-more-page">
-    <Heading pretext="Learn more">
-      <span>Find out how we intend to do deliver on our promises. Or don't. It's up to you really.</span>
+  <div id="features-page">
+    <!-- Heading -->
+    <Heading pretext="Features">
+      We've packed a lot into this.
+      <br />Take a look at the features available.
     </Heading>
 
-    <!-- Section #1 -->
-    <div class="section how">
-      <PatternBG text="How How How" repeat="1" />
-      <div class="square"></div>
-      <div class="title">
-        <span>How does it all work?</span>
-        <div class="line"></div>
-      </div>
-      <div class="description">
-        <span>It doesn't. At least now it does't because we have not figured out how it would work. Probably some location tracking, some AI, some blockchain, deep-learning and other buzzwords that get you to like what we have done in theory.</span>
-      </div>
-      <div class="cards-wrapper">
-        <div class="container-title">Here are some FAQs for you:</div>
-        <div class="cards-container">
-          <Card
-            title="Is my data saved?"
-            description="Naturally. We collect data and then run ads tailord to you on our platform. We might even sell some of that data for sweet sweet cash."
-          />
-          <Card title="How can I rate?" description="You can't and you shouldn't." />
-          <Card title="What counts as a trip?" description="LCD, DMT, Herion, Crack Cocaine, etc." />
-          <Card
-            title="How does the app predict delays?"
-            description="By the power of Indian YouTubers who have taught me how to do AI."
-          />
+    <!-- Features -->
+    <div class="features-overview">
+      <PatternBG text="Features" repeat="3" />
+      <div class="left">
+        <div class="feature-short">
+          <div class="text">Heat Maps</div>
+          <div class="circle"></div>
+        </div>
+        <div class="feature-short middle">
+          <div class="text">Rating System</div>
+          <div class="circle"></div>
+        </div>
+        <div class="feature-short">
+          <div class="text">Trip Advising System</div>
+          <div class="circle"></div>
         </div>
       </div>
+
+      <img class="mockup" src="../assets/mockup.png" />
+
+      <div class="right">
+        <div class="feature-short">
+          <div class="text">Journey History</div>
+          <div class="circle"></div>
+        </div>
+        <div class="feature-short middle">
+          <div class="text">Travel Info/Warnings</div>
+          <div class="circle"></div>
+        </div>
+        <div class="feature-short">
+          <div class="text">New! Rate your Conductor</div>
+          <div class="circle"></div>
+        </div>
+      </div>
+
+      <div class="square first"></div>
+      <div class="square second"></div>
     </div>
 
-    <!-- Section #2 -->
-    <div class="section why">
-      <PatternBG text="Rate Rate Rate" repeat="1" />
-      <div class="title">
-        <span>Why should you rate?</span>
-        <div class="line"></div>
-      </div>
-      <div class="description">
-        <span>I really don't know. Maybe you shouldn't rate if you don't want to. But guess what - the more you rate, the more data we have and next time your train is delayed, you will know, and you will choose an alternative route, maybe even one we have created fot you, who knows. But one thing is for sure, rate so you are not late!</span>
-      </div>
-      <div class="rate-prompt">
-        <Reason
-          v-for="(reason, index) in $store.state.reasons"
-          :key="index"
-          :text="reason.text"
-          :explanation="reason.explanation"
-          :index="index"
-        />
-      </div>
-    </div>
-
-    <!-- Secion #3 -->
-    <div class="survery-results">
-      <div class="headline">
-        <span>
-          After condicting an extensive research and asking over 100 people if they would use the app, we have concluded that coninuing with this project is not advisiable
-          <span
-            class="dot"
-          >.</span>
-        </span>
-      </div>
-      <div class="line"></div>
-      <div class="text">
-        <span>Pretty much everyone hated the public transit but are willing to do nothing about it because they are complacent little bitches.</span>
-      </div>
+    <!-- Features in details -->
+    <div class="features-details-container">
+      <div class="square"></div>
+      <FeatureDetails
+        v-for="(feature, index) in $store.state.features"
+        :key="feature.name"
+        :name="feature.name"
+        :description="feature.description"
+        :illustrationURL="feature.illustrationURL"
+        :color="index%2==0 ? 'first' : 'second'"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import Heading from "./../components/common/Heading";
 import PatternBG from "./../components/common/PatternBG";
-import Card from "./../components/learn-more-components/Card";
-import Reason from "./../components/learn-more-components/Reason";
-
-export default { components: { Heading, PatternBG, Card, Reason } };
+import Heading from "./../components/common/Heading";
+import FeatureDetails from "./../components/features-components/FeatureDetails.vue";
+export default {
+  components: { PatternBG, FeatureDetails, Heading }
+};
 </script>
 
 <style lang="scss" scoped>
-@import "./../styles/learnmore.scss";
+@import "../styles/features.scss";
+</style>
 </style>
